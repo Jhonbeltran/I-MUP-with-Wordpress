b7657b477d38971803a09e0613ee4c1bd0ea0783
Carlos <carlo.6a@gmail.com>
2016-12-29 10:30:13 -0500

    portafolio

--- wp-content/themes/imup_web/index.php
+++ wp-content/themes/imup_web/index.php
@@ -439,7 +439,7 @@
                   <?php $the_query = new WP_Query( 'cat=3&showposts=10' ); ?>
                   <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
 
-                    <div class="ch-item ch-item-hover"><?php if ( has_post_thumbnail() ) { the_post_thumbnail( '' ); } ?>    </div>
+                    <div class="ch-item ch-item-hover"><?php if ( has_post_thumbnail() ) { the_post_thumbnail( '' ); } ?>
 
                 <div class="ch-info">
                     <div class="ch-info-icons">
@@ -510,7 +510,7 @@
                         <div class="clear"></div>
                     </div>
                 </div>
-
+            </div>
                <?php endwhile;?>
         </li>
         <hr />
