a0a01d0d98505d5ebadc79e57a0123874f172a95
Carlos <carlo.6a@gmail.com>
2016-12-29 11:18:07 -0500

    portafolio

--- wp-content/themes/imup_web/index.php
+++ wp-content/themes/imup_web/index.php
@@ -439,7 +439,7 @@
                   <?php $the_query = new WP_Query( 'cat=3&showposts=10' ); ?>
                   <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
 
-                    <div class="ch-item ch-item-hover" style="background: url('<?php bloginfo ('template_directory'); ?>/images/portfolios/001-260x172.jpg') no-repeat center;">
+                    <div class="ch-item ch-item-hover"><?php if ( has_post_thumbnail() ) { the_post_thumbnail( '' ); } ?>
 
                 <div class="ch-info">
                     <div class="ch-info-icons">
