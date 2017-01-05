96c86bf0c7c54e174e018bd8b3fbb7e6b1553e01
Carlos <carlo.6a@gmail.com>
2017-01-05 15:13:40 -0500

    CSS

--- wp-content/themes/imup_web/index.php
+++ wp-content/themes/imup_web/index.php
@@ -430,7 +430,7 @@
 <div id="page-meta" class="container">
    <!-- SLOGAN -->
     <div class="slogan">
-        <h2>Featured Projects</h2><h3>a quick look to my wonderful works.</h3>
+        <h2>PROYECTOS</h2><h3>a quick look to my wonderful works.</h3>
     </div>
 </div>
 <!-- END PAGE META -->
