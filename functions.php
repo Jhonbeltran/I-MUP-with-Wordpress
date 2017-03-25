97d7d12310a9502cb91395f080f2d64d08e0b8ba
Carlos <carlo.6a@gmail.com>
2017-03-25 12:52:08 -0500

    to update
    New Codes

--- functions.php
+++ functions.php
@@ -1,21 +1,13 @@
 <?php
-/**
- * Your Inspiration Themes
- * 
- * @package WordPress
- * @subpackage Your Inspiration Themes
- * @author Your Inspiration Themes Team <info@yithemes.com>
- *
- * This source file is subject to the GNU GENERAL PUBLIC LICENSE (GPL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://www.gnu.org/licenses/gpl-3.0.txt
- */
- 
-//let's start the game! 
-require_once('core/load.php');
+register_nav_menus( array(
+  'primary'   => esc_html__( 'Primary Menu', 'regina-lite' ),
+  'secondary' => esc_html__( 'Secondary Menu', 'regina-lite' ),
+) );
 
-//---------------------------------------------
-// Everybody changes above will lose his hands
-//---------------------------------------------
 
+
+ if ( function_exists( 'add_theme_support' ) ) {
+    add_theme_support( 'post-thumbnails' );
+    //300 pixels wide (and unlimited height)
+}
+ ?>
